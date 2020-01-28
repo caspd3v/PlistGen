4a8e7a3d85740d17ac211f16751865f66a1450e8
CaspD3V <34457433+caspd3v@users.noreply.github.com>
2020-01-28 17:45:45 -0500

    release

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# PlistGen
-A simple plist generator written in python.
+#PlistGen
+
+PlistGen is a tool to generate plists for cydia alternatives to link your ipas to your app.
+
+##Permissions
+
+cd into PlistGen's directory.
+
+'''bash
+chmod +x plistgen.py
+'''
+
+##Running
+
+cd into PlistGen's directory.
+
+Mac & Linux
+
+'''bash
+python3 plistgen.py
+'''
+
+cd into PlistGen's directory.
+
+'''bash
+C:\Windows\py.exe plistgen.py
+'''
+
+##Feilds
+
+1. .plist Name: "The name of the plist when its generated."
+2. .ipa Link: "The link to your .ipa file on a server."
+3. .plist's Url: "The full .ipa url including the directory."
+4. Bundle ID: "The .ipa bundle id." Example: co.eonhub.app
+5. Label: "The text that comes up when you click install on your iDevice."
+
+##Licence
+
+[Casp's Website](https://casp.dev)
---
+++ plistgen.py
@@ -0,0 +1,29 @@
+#Copyright CaspD3V All Rights reserved.
+#This script can generate plists for cydia alternatives.
+#Dont steal this code and say its yours because that will make you a skid..
+
+print ("--------------------------")
+print ("")
+print ("Welcome!")
+print ("Lets generate some plists.")
+print ("--------------------------")
+plistName = (input(".plist Name: "))
+ipaLink = (input(".ipa Link: "))
+plistUrl = (input(".plist's Url: "))
+bundleId = (input("Bundle ID: "))
+appLabel = (input("Label: "))
+
+print("")
+print ("Creating Plist..")
+
+f = open(''+plistName+'.plist',"w+")
+
+for i in range(1):
+     f.write('<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd …">\n<plist version="1.0">\n<dict>\n<key>items</key>\n<array>\n<dict>\n<key>assets</key>\n<array>\n<dict>\n<key>kind</key>\n<string>software-package</string>\n<key>url</key>\n<string>'+ipaLink+'</string>\n</dict>\n<dict>\n<key>kind</key>\n<string>display-image</string>\n<key>url</key>\n<string>https://app.eonhub.co/img/icon.png</string>\n</dict>\n<dict>\n<key>kind</key>\n<string>full-size-image</string>\n<key>url</key>\n<string>https://app.eonhub.co/img/icon.png</string>\n</dict>\n</array>\n<key>metadata</key>\n<dict>\n<key>bundle-identifier</key>\n<string>'+bundleId+'</string>\n<key>bundle-version</key>\n<string>1</string>\n<key>kind</key>\n<string>software</string>\n<key>title</key>\n<string>'+appLabel+'</string>\n</dict>\n</dict>\n</array>\n</dict>\n</plist> \r' )
+f.close() 
+
+print ("Done.")
+
+print ("--------------------------")
+print ("Install Link:")
+print ('itms-services://?action=download-manifest&url='+plistUrl+'.plist')
